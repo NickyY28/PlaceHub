4dfa516640b493a3f1f6dc0628614f603cca1adf
Nicky Yadav <nicky28bt@gmail.com>
2026-07-11 20:01:30 +0530

    add Bootstrap icons and Pinia for state management in the frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 
 // Import Bootstrap CSS and JS
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
 
 createApp(App).mount("#app");
+
+app.use(createPinia());
